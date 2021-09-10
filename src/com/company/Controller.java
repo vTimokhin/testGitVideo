b592c98864656d4a699c027fdbb4b779703ed66e
vTim <jofill@mail.ru>
2021-09-10 20:35:42 +0300

    First commit

---
+++ src/com/company/Controller.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+            Pane root = loader.load();
+            Controller controller = loader.getController();
+            controller.setStage(stage);
+            Scene scene = new Scene(root);
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle("Title");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        launch(args);
+    }
+}
